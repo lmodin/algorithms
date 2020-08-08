5e919e4f33b72cf3af01db1ece89fc33a3a74840
lmodin <lisamodin@live.com>
2020-08-08 11:11:54 -0700

    Adding mergeSort, islandCount, insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,71 @@
+/**
+ * Insertion sort is a basic sorting algorithm.
+ *
+ * Insertion sort iterates over an array, growing a sorted array behind the current location.
+ * It takes each element from the input and finds the spot, up to the current point,
+ * where that element belongs. It does this until it gets to the end of the array.
+ *
+ * Insertion sort should be implemented as a stable sort. This means that equal elements
+ * should retain their relative order. Numbers, as primitives, give us no way to check this,
+ * so we'll be sorting objects with a value field, on which they will be sorted, like so:
+ *
+ * `[{value: 10}, {value: 5, order: 1}, {value: 5, order: 2}]`
+ *
+ * A stable sort must return `{value: 5, order: 1}, {value:5, order: 2}` in that order.
+ *
+ * ---
+ *
+ * EXTRA CREDIT:
+ *
+ * Refactor your sort to (optionally) take an explicit comparator function
+ * as its second argument, so that callers can define arbitrary ways to sort elements.
+ * See [Array.prototype.sort](http://devdocs.io/javascript/global_objects/array/sort)
+ * for an example of how this works (excerpt below):
+ *
+ * > If `comparator(a, b)` is less than `0`, sort `a` to a lower index than `b`, i.e. `a` comes first.
+ * > If `comparator(a, b)` returns `0`, leave `a` and `b` unchanged with respect to each other, but sorted with respect to all different elements.
+ * > If `comparator(a, b)` is greater than `0`, sort `b` to a lower index than `a`.
+ *
+ * If no comparator is given, just sort the elements using `<` or `>`.
+ **/
+
+// Example usage:
+// insertionSort([{value: 2}, {value: 1}, {value: 3}]);
+// yields [{value: 1}, {value: 2}, {value: 3}]
+
+// This function is to help you test, and should not be incorporated in your solution.
+// It will transform an array of numbers into an array of valid objects.
+var testingTransform = function(array) {
+  var transform = [];
+
+  for (var i = 0; i < array.length; i++) {
+    transform.push({value: array[i], i: i});
+  }
+
+  return transform;
+};
+
+var insertionSort = function(array) {
+  // iterate through the array, using for loop
+  for (var i = 0; i < array.length; i++) {
+    //set variable equal to value at the current location
+    var value = array[i].value;
+    //iterate from the start of the array to the current location
+    for (var v = 0; v < i; v++) {
+      //if our stored value is smaller than the current iteration's value => this will also avoid having to look at the second value in the object but will keep the order
+      if (value < array[v].value) {
+        //insert the current element at this point
+        array.splice(v, 0, array[i]);
+        //remove current element from original location, which has now increased since we've added an element before i
+        array.splice(i + 1, 1);
+        //break our loop and look at the next value
+        break;
+      }
+    }
+  }
+  return array;
+};
+
+var test = insertionSort([{value: 2}, {value: 1}, {value: 3}]);
+//console.log(insertionSort(test)[2])
+//yields [{value: 1}, {value: 2}, {value: 3}]
